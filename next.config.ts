8355b72d87c7d584ceeabd1a8045a37c1b1acfc1
Zubair Ibn Zamir <zizdjbd@gmail.com>
2026-03-05 17:30:51 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,13 @@ export default nextConfig;
 
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
+
+for (let i = 65; i <= 90; i++) {
+  console.log(String.fromCharCode(i));
+}
+
+console.log(process.env.VERCEL);
+
 if (!process.env.VERCEL) {
   initOpenNextCloudflareForDev();
 }
