c12b48c0d0de107ebd02daed514bdd4a2d92659a
Zubair Ibn Zamir <zizdjbd@gmail.com>
2026-03-05 16:31:16 +0600

    zubair

--- app/proxy.ts
+++ app/proxy.ts
@@ -2,6 +2,6 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export function proxy(request: NextRequest) {
   console.log("Proxy hit:", request.nextUrl.pathname);
-
+  console.log('zubair')
   return NextResponse.next();
 }
